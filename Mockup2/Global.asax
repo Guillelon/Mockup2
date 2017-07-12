e39bf771940d45ffe50cabdd01ddca5c60f9423d
Guillermo Oramas <guillermo.oramas.1@gmail.com>
2017-07-11 19:07:10 -0500

    init commit

---
+++ Mockup2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mockup2.MvcApplication" Language="C#" %>
---
+++ Mockup2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mockup2.Startup))]
+namespace Mockup2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
